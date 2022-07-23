becd0e3f76b1da47c52041134cae746d783f39a9
Asif Pervez Polok <104290708+polok-dev98@users.noreply.github.com>
2022-07-23 13:18:02 +0100

    Add files via upload

---
+++ seq2seq_chatbot.py
@@ -0,0 +1,325 @@
+#importing library
+import re
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pickle
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Embedding, LSTM, Input
+import pickle
+import numpy as np
+import pandas as pd
+
+#load the dataset
+lines = open('movie_lines.txt', encoding='utf-8',
+             errors='ignore').read().split('\n')
+
+convers = open('movie_conversations.txt', encoding='utf-8',
+             errors='ignore').read().split('\n')
+
+
+# text preprocessing
+exchn = []
+for conver in convers:
+    exchn.append(conver.split(' +++$+++ ')[-1][1:-1].replace("'", " ").replace(",","").split())
+
+diag = {}
+for line in lines:
+    diag[line.split(' +++$+++ ')[0]] = line.split(' +++$+++ ')[-1]
+
+## delete
+del(lines, convers, conver, line)
+
+questions = []
+answers = []
+
+for conver in exchn:
+    for i in range(len(conver) - 1):
+        questions.append(diag[conver[i]])
+        answers.append(diag[conver[i+1]])
+
+
+sorted_ques = []
+sorted_ans = []
+for i in range(len(questions)):
+    if len(questions[i]) < 13:
+        sorted_ques.append(questions[i])
+        sorted_ans.append(answers[i])
+
+# Text cleaning
+
+def clean_text(txt):
+    txt = txt.lower()
+    txt = re.sub(r"i'm", "i am", txt)
+    txt = re.sub(r"he's", "he is", txt)
+    txt = re.sub(r"she's", "she is", txt)
+    txt = re.sub(r"that's", "that is", txt)
+    txt = re.sub(r"what's", "what is", txt)
+    txt = re.sub(r"where's", "where is", txt)
+    txt = re.sub(r"\'ll", " will", txt)
+    txt = re.sub(r"\'ve", " have", txt)
+    txt = re.sub(r"\'re", " are", txt)
+    txt = re.sub(r"\'d", " would", txt)
+    txt = re.sub(r"won't", "will not", txt)
+    txt = re.sub(r"can't", "can not", txt)
+    txt = re.sub(r"[^\w\s]", "", txt)
+    return txt
+
+clean_ques = []
+clean_ans = []
+
+for line in sorted_ques:
+    clean_ques.append(clean_text(line))
+        
+for line in sorted_ans:
+    clean_ans.append(clean_text(line))
+
+
+for i in range(len(clean_ans)):
+    clean_ans[i] = ' '.join(clean_ans[i].split()[:11])
+
+
+
+# trimming
+clean_ans=clean_ans[:30000]
+clean_ques=clean_ques[:30000]
+
+
+# count vectorizer building
+word2count = {}
+
+for line in clean_ques:
+    for word in line.split():
+        if word not in word2count:
+            word2count[word] = 1
+        else:
+            word2count[word] += 1
+for line in clean_ans:
+    for word in line.split():
+        if word not in word2count:
+            word2count[word] = 1
+        else:
+            word2count[word] += 1
+
+# remove less frequent word
+thresh = 5
+
+vocab = {}
+word_num = 0
+for word, count in word2count.items():
+    if count >= thresh:
+        vocab[word] = word_num
+        word_num += 1
+        
+# Adding start of string and end of string tokens         
+for i in range(len(clean_ans)):
+    clean_ans[i] = '<SOS> ' + clean_ans[i] + ' <EOS>'
+
+
+# Assign the unique number of these token .
+tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
+x = len(vocab)
+for token in tokens:
+    vocab[token] = x
+    x += 1
+    
+    
+
+vocab['cameron'] = vocab['<PAD>']
+vocab['<PAD>'] = 0
+
+
+# inv answers dict biulding
+inv_vocab = {w:v for v, w in vocab.items()}
+
+
+# Building the word vectorizer for model input
+encoder_inp = []
+for line in clean_ques:
+    lst = []
+    for word in line.split():
+        if word not in vocab:
+            lst.append(vocab['<OUT>'])
+        else:
+            lst.append(vocab[word])
+        
+    encoder_inp.append(lst)
+
+decoder_inp = []
+for line in clean_ans:
+    lst = []
+    for word in line.split():
+        if word not in vocab:
+            lst.append(vocab['<OUT>'])
+        else:
+            lst.append(vocab[word])        
+    decoder_inp.append(lst)
+
+# Add padding 
+encoder_inp = pad_sequences(encoder_inp, 13, padding='post', truncating='post')
+decoder_inp = pad_sequences(decoder_inp, 13, padding='post', truncating='post')
+
+
+
+
+decoder_final_output = []
+for i in decoder_inp:
+    decoder_final_output.append(i[1:]) 
+
+decoder_final_output = pad_sequences(decoder_final_output, 13, padding='post', truncating='post')
+
+
+# Convert into 3D vector and One hot encoder of each vocabulary
+
+decoder_final_output = to_categorical(decoder_final_output, len(vocab))
+
+########################### Model building ##################################### 
+
+# Building the Encoder
+enc_inp = Input(shape=(13, ))
+dec_inp = Input(shape=(13, ))
+
+
+VOCAB_SIZE = len(vocab)
+embed = Embedding(VOCAB_SIZE+1, output_dim=50, 
+                  input_length=13,
+                  trainable=True                  
+                  )
+
+
+enc_embed = embed(enc_inp)
+enc_lstm = LSTM(400, return_sequences=True, return_state=True)
+enc_op, h, c = enc_lstm(enc_embed)
+enc_states = [h, c]
+
+# Building the Decoder
+
+dec_embed = embed(dec_inp)
+dec_lstm = LSTM(400, return_sequences=True, return_state=True)
+dec_op, _, _ = dec_lstm(dec_embed, initial_state=enc_states)
+
+dense = Dense(VOCAB_SIZE, activation='softmax')
+
+dense_op = dense(dec_op)
+
+model = Model([enc_inp, dec_inp], dense_op)
+
+
+# Compile and Train the model
+
+model.compile(loss='categorical_crossentropy',metrics=['acc'],optimizer='adam')
+#Try more epochs for better result
+model.fit([encoder_inp, decoder_inp],decoder_final_output,epochs=30)
+
+########################### Create the Interference model-############################
+
+
+#Encoder
+enc_model = Model([enc_inp], enc_states)
+
+# decoder Model
+decoder_state_input_h = Input(shape=(400,))
+decoder_state_input_c = Input(shape=(400,))
+
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+
+
+decoder_outputs, state_h, state_c = dec_lstm(dec_embed , 
+                                    initial_state=decoder_states_inputs)
+
+
+decoder_states = [state_h, state_c]
+
+
+dec_model = Model([dec_inp]+ decoder_states_inputs,
+                                      [decoder_outputs]+ decoder_states)
+
+############### Create the prediction and chat with the model-##################
+
+prepro1 = ""
+while prepro1 != 'q':
+    prepro1  = input("you : ")
+    ## prepro1 = "Hello"
+
+    prepro1 = clean_text(prepro1)
+    ## prepro1 = "hello"
+
+    prepro = [prepro1]
+    ## prepro1 = ["hello"]
+
+    txt = []
+    for x in prepro:
+        # x = "hello"
+        lst = []
+        for y in x.split():
+            ## y = "hello"
+            try:
+                lst.append(vocab[y])
+                ## vocab['hello'] = 454
+            except:
+                lst.append(vocab['<OUT>'])
+        txt.append(lst)
+
+    ## txt = [[454]]
+    txt = pad_sequences(txt, 13, padding='post')
+
+    ## txt = [[454,0,0,0,.........13]]
+
+    stat = enc_model.predict( txt )
+
+    empty_target_seq = np.zeros( ( 1 , 1) )
+     ##   empty_target_seq = [0]
+
+
+    empty_target_seq[0, 0] = vocab['<SOS>']
+    ##    empty_target_seq = [255]
+
+    stop_condition = False
+    decoded_translation = ''
+
+    while not stop_condition :
+
+        dec_outputs , h, c= dec_model.predict([ empty_target_seq] + stat )
+        decoder_concat_input = dense(dec_outputs)
+        ## decoder_concat_input = [0.1, 0.2, .4, .0, ...............]
+
+        sampled_word_index = np.argmax( decoder_concat_input[0, -1, :] )
+        ## sampled_word_index = [2]
+
+        sampled_word = inv_vocab[sampled_word_index] + ' '
+
+        ## inv_vocab[2] = 'hi'
+        ## sampled_word = 'hi '
+
+        if sampled_word != '<EOS> ':
+            decoded_translation += sampled_word  
+
+        if sampled_word == '<EOS> ' or len(decoded_translation.split()) > 13:
+            stop_condition = True 
+
+        empty_target_seq = np.zeros( ( 1 , 1 ) )  
+        empty_target_seq[ 0 , 0 ] = sampled_word_index
+        ## <SOS> - > hi
+        ## hi --> <EOS>
+        stat = [h, c]  
+
+    print("chatbot attention : ", decoded_translation )
+    print("==============================================")  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
